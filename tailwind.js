458850fcb61e1b52a1c38ed1d2b48e539fad02e3
Idongesit Ntuk <ideeofficial@gmail.com>
2019-03-16 23:24:56 +0100

    Add logo; add pattern on hero background

--- tailwind.js
+++ tailwind.js
@@ -773,6 +773,7 @@ module.exports = {
     'lg': '0 15px 30px 0 rgba(0,0,0,0.11), 0 5px 15px 0 rgba(0,0,0,0.08)',
     'inner': 'inset 0 2px 4px 0 rgba(0,0,0,0.06)',
     'outline': '0 0 0 3px rgba(52,144,220,0.5)',
+    'raised': '0 18px 35px 0 rgba(42,51,83,.12),0 8px 15px rgba(0,0,0,.06) !important',
     'none': 'none',
   },
 
